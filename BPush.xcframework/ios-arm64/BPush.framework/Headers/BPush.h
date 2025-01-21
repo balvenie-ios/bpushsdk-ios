7ca334d4145d14e5e6c6c37febd6aef97f0197e0
weichen cheng <weichen.cheng@baifu-tech.net>
2025-01-21 16:36:40 +0800

    1. 重包static framework 2. update podspec version

---
+++ BPush.xcframework/ios-arm64/BPush.framework/Headers/BPush.h
@@ -0,0 +1,18 @@
+//
+//  BPush.h
+//  BPush
+//
+//  Created by weichen cheng on 2025/1/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BPush.
+FOUNDATION_EXPORT double BPushVersionNumber;
+
+//! Project version string for BPush.
+FOUNDATION_EXPORT const unsigned char BPushVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BPush/PublicHeader.h>
+
+
